f7ecd73a31404c0d314c6da26338f8862159abf3
AllenCoder <AllenCoder@126.com>
2017-06-27 12:46:23 +0800

    Initial commit

---
+++ src/main/java/com/allen/demo/Greeting.java
@@ -0,0 +1,22 @@
+package com.allen.demo;
+
+/**
+ * Created by Allen on 2017/6/27.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/com/allen/demo/GreetingController.java
@@ -0,0 +1,18 @@
+package com.allen.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Allen on 2017/6/27.
+ */
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
